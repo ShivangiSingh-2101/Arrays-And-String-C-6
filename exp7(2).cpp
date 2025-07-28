f46fc1a6e40cf48a5443710719e8ac3e02b040cf
Shivangi Singh <Shivangisingh212005@gmail.com>
2025-07-28 15:20:59 +0530

    Update exp7(2).cpp

--- exp7(2).cpp
+++ exp7(2).cpp
@@ -1,22 +1,28 @@
-//Shivangi Singh
-//prn-2070123148
-//B3
+// Shivangi Singh
+// prn-2070123148
+// B3
+
 #include <iostream>
 using namespace std;
 
-int main() {
-    int arr[] = {21, 18, 36, 32, 44};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    int target;
+int array[5];
 
+int main() {
+    
+    for (int i = 0; i < 5; i++) {
+        cout << "Enter the element " << i + 1 << ": ";
+        cin >> array[i];
+    }
+
+    int target;
     cout << "Enter the number to find: ";
     cin >> target;
 
     bool found = false;
     int position = -1;
 
-    for (int i = 0; i < size; i++) {
-        if (arr[i] == target) {
+    for (int i = 0; i < 5; i++) {
+        if (array[i] == target) {
             found = true;
             position = i;
             break;  
@@ -31,8 +37,11 @@ int main() {
     return 0;
 }
 /*Output
+Enter the element 1: 10
+Enter the element 2: 20
+Enter the element 3: 36
+Enter the element 4: 40
+Enter the element 5: 50
 Enter the number to find: 36
 36 found at position 2 (0-based index).
-Enter the number to find: 50
-50 not found in the array.
 */
