4e940d56301bac0022beddbab8c213a1ce4f00dd
Shivangi Singh <Shivangisingh212005@gmail.com>
2025-07-28 15:24:45 +0530

    Update exp7(3).cpp

--- exp7(3).cpp
+++ exp7(3).cpp
@@ -1,22 +1,27 @@
-//Shivaangui Singh 
+//Shivangi Singh
 //prn-24070123148
 //B3
-
 #include <iostream>
 using namespace std;
 
 int main() {
-    int arr[] = {21, 18, 36, 32, 44};
-    int size = sizeof(arr) / sizeof(arr[0]);
+    const int size = 5;   // size of the array
+    int arr[size];
     int sum = 0;
     float average;
 
- 
+    // Input array elements from user
+    for (int i = 0; i < size; i++) {
+        cout << "Enter element " << (i + 1) << ": ";
+        cin >> arr[i];
+    }
+
+    // Calculate sum
     for (int i = 0; i < size; i++) {
         sum += arr[i];
     }
 
-  
+    // Calculate average
     average = (float)sum / size;
 
     cout << "Sum of array elements: " << sum << endl;
@@ -24,6 +29,7 @@ int main() {
 
     return 0;
 }
+
 /*Output
 Sum of array elements: 151
 Average of array elements: 30.2
